b94c64b189ec3e25298f59d8dda724a7f682cf59
shravan20 <mrshravankumarb@gmail.com>
2025-06-28 10:44:49 +0530

    Backup before complete rebuild

--- next.config.js
+++ next.config.js
@@ -3,10 +3,14 @@ const sourcebit = require('sourcebit');
 
 const sourcebitConfig = require('./sourcebit.js');
 
+
 sourcebit.fetch(sourcebitConfig);
 
 module.exports = {
     trailingSlash: true,
+    devIndicators: {
+        autoPrerender: false
+    },
     sassOptions: {
         // scss files might import plain css files from the "public" folder:
         // @import "example.css";
