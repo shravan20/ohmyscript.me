ca4136a44c455f84fa6409ee4c4fa7604a43e176
shravan20 <mrshravankumarb@gmail.com>
2025-06-28 21:00:47 +0530

    Update home.js

--- src/layouts/home.js
+++ src/layouts/home.js
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Layout, Hero, TechStack, Projects, Articles, Newsletter } from '../components';
+import { Layout, Hero, TechStack, StackLife, Projects, Articles, Newsletter } from '../components';
 
 export default function HomePage(props) {
     return (
         <Layout {...props}>
             <Hero data={props.data} />
             <TechStack />
+            <StackLife />
             <Projects />
             <Articles pages={props.pages} />
             <Newsletter />
